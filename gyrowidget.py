9c4a26e9d3b49440bf0098bc36eaddd68f54760f
Gregory Haynes <greg@greghaynes.net>
2011-11-13 04:04:22 -0800

    Added labels to gyro widgets

--- gyrowidget.py
+++ gyrowidget.py
@@ -2,6 +2,14 @@ from PyQt4 import QtGui
 
 import angledial
 
+class GyroSingleAxisWidget(QtGui.QWidget):
+	def __init__(self, dial, label):
+		super(GyroSingleAxisWidget, self).__init__()
+		v_layout = QtGui.QVBoxLayout()
+		v_layout.addWidget(QtGui.QLabel(label))
+		v_layout.addWidget(dial)
+		self.setLayout(v_layout)
+
 class GyroWidget(QtGui.QWidget):
 	def __init__(self):
 		super(GyroWidget, self).__init__()
@@ -12,9 +20,13 @@ class GyroWidget(QtGui.QWidget):
 
 	def setupUi(self):
 		self.roll_dial = angledial.AngleDial()
+		self.pitch_dial = angledial.AngleDial()
+		self.yaw_dial = angledial.AngleDial()
 
 		h_layout = QtGui.QHBoxLayout()
-		h_layout.addWidget(self.roll_dial)
+		h_layout.addWidget(GyroSingleAxisWidget(self.roll_dial, "Roll"))
+		h_layout.addWidget(GyroSingleAxisWidget(self.pitch_dial, "Pitch"))
+		h_layout.addWidget(GyroSingleAxisWidget(self.yaw_dial, "Yaw"))
 
 		self.setLayout(h_layout)
 
