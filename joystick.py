8e50e131c481b9cedee4391808c58e762fc05710
Gregory Haynes <greg@greghaynes.net>
2012-01-21 19:28:09 -0800

    Joystick creates and scales events

--- joystick.py
+++ joystick.py
@@ -1,4 +1,5 @@
 import os
+import struct
 from PyQt4 import QtGui, QtCore
 
 def available_joysticks():
@@ -17,16 +18,27 @@ class JoystickEvent(object):
 
 class QJoystick(QtCore.QSocketNotifier):
 	# QJoystick.gotEvent(JoystickEvent)
-	gotEvent = QtCore.pyqtSignal()
+	gotEvent = QtCore.pyqtSignal(JoystickEvent)
+	maxvals = {}
 
 	def __init__(self, joystick_file):
 		self.joystick_file = super(QJoystick, self).__init__(joystick_file.fileno(), 0)
 		self.joystick_file = joystick_file
 		self.setEnabled(True)
+		self.activated.connect(self.onRead)
 
 	def onRead(self, sock):
-		ev = JoystickEvent(*struct.unpack("IhBB", self.joystick_file.read()))
-		self.gotEvent.emit(ev)
+		bin_data = struct.unpack("IhBB", os.read(sock, 8))
+		ev = JoystickEvent(*bin_data)
+		if ev.event_type not in self.maxvals:
+			self.maxvals[ev.event_type] = {}
+
+		try:
+			maxval = self.maxvals[ev.event_type][ev.number]
+			ev.value = ev.value / 32768.0
+			self.gotEvent.emit(ev)
+		except KeyError:
+			self.maxvals[ev.event_type][ev.number] = ev.value
 
 class OpenJoystickDialog(QtGui.QDialog):
 	def __init__(self):
